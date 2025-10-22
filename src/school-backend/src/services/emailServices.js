df5e404d0d061afd6d11bae865c08c63f5b43da5
rltsolution2025 <social.rltdigital@gmail.com>
2025-10-22 15:44:00 +0530

    nodemailer updated

--- src/school-backend/src/services/emailServices.js
+++ src/school-backend/src/services/emailServices.js
@@ -7,8 +7,8 @@ dotenv.config();
 // ----------------------
 const getTransporter = () => nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465
+  port: 587,
+  secure: false, // use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // App password
